ed664d60c6277aa68e80444defa98b9542c6ec63
Dhiego Martins Andrade <mdhiego@gmail.com>
2021-04-11 07:50:09 -0300

    Fix pti-solution

--- pti-solution/index.js
+++ pti-solution/index.js
@@ -1,27 +1,30 @@
+console.log("Hi! Welcome!");
+console.log("This little calculator indicates the fuel which is " +
+    "most worth it today in terms of consume!\n");
+
+console.log("Let's go check!\n");
+
+let isThereAnotherCheck;
+
 do {
-    console.log("Hi! Welcome!");
-    console.log("This little calculator indicates the fuel which is" +
-        "most worth it today in terms of consume!\n");
-
-    console.log("Let's go check!\n");
-
     let gasolineCost = prompt("How much is gasoline costing?");
-    let alcoholCost = prompt("How much is alcohol costing?\n");
+    let alcoholCost = prompt("How much is alcohol costing?");
 
     const equilibriumRatio = 0.7;
     let costRatio = alcoholCost / gasolineCost;
 
-    if (costRatio < equilibriumRatio) {
-        console.log("Gasoline is the option which most worth it!\n");
-    } else if (costRatio > equilibriumRatio) {
-        console.log("Alcohol is the option which most worth it!\n");
+    console.log(costRatio);
+
+    if (costRatio > equilibriumRatio) {
+        console.log("\nGasoline is the option which most worth it!\n");
+    } else if (costRatio < equilibriumRatio) {
+        console.log("\nAlcohol is the option which most worth it!\n");
     } else {
-        console.log("Doesn't matter, both options worth the same!\n");
+        console.log("\nDoesn't matter, both options worth the same!\n");
     }
 
-    let toContinue;
     do {
-        toContinue = prompt("Do you want to make another check (y/n)?");
-    } while (toContinue != 'y' && toContinue != 'n');
+        isThereAnotherCheck = prompt("Do you want to make another check (y/n)?");
+    } while (isThereAnotherCheck != 'y' && isThereAnotherCheck != 'n');
 
-} while (toContinue == 'y');
+} while (isThereAnotherCheck == 'y');
